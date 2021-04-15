30cf4f89d395f8e31dd2128764dba22a8f896c85
Harshdeep Dhiman <dhiman.harshdeep18@gmail.com>
2021-04-15 18:31:32 +0530

    very init

---
+++ backend/data/products.js
@@ -0,0 +1,52 @@
+const products = [
+  {
+    name: 'Samsung Galaxy M21',
+    image: '/images/m21.jpg',
+    description:
+      'With the Samsung Galaxy M21, Samsung provide the powerful 6000 mAh battery along with all round features comprising of a 48 MP rear camera in Triple camera set up and 20 MP front camera and an immersive sAmoled screen ',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 13000,
+    countInStock: 3,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'iPhone 12',
+    image: '/images/phone.jpg',
+    description:
+      'Introducing the iPhone 12. A transformative triple-camera system that adds tons of capability without complexity. An unprecedented leap in battery life',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 80000,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Fender CD-60SCE Acoustic-Electric Guitar (Mahagony)',
+    image: '/images/Fender.jpg',
+    description:
+      'Combining powerful onboard electronics-including a built-in tuner-with great tone and easy playability, the cd-60sce all-mahogany is ideal for beginning to intermediate level players who are ready to plug in. Featuring a single-cutaway body for easy upper-fret access, warm-sounding solid mahogany top, easy-to-play neck and mahogany back and sides, the cd-60sce is perfect for the couch, the beach or the coffeehouse-anywhere you want classic Fender playability and sound. All electronic products must be used with a step down/up converter for indian voltage compatibility.',
+    brand: 'Cannon',
+    category: 'Music',
+    price: 25000,
+    countInStock: 0,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Sony Playstation 5 White Version',
+    image: '/images/ps5.jpg',
+    description:
+      'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
+    brand: 'Sony',
+    category: 'Electronics',
+    price: 50000,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  }
+]
+
+export default products
